7e7291a7bdbfbeb93c6338c1ba8233e49f0f3204
Max <mmm@parametrica.team>
2020-10-21 19:41:22 +0300

    Добавьте файлы проекта.

---
+++ Flat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinInsolGenerator
+{
+    class Flat
+    {
+        public string Id { get; set; }
+        public List<int> Indexes { get; set; }
+
+        /// <summary>
+        /// CL, CR, MD, MU, llu
+        /// </summary>
+        public string FType { get; set; }
+        public int TopWindows { get; set; }
+        public int BottomWindows { get; set; }
+
+        public Flat() { }
+        public Flat(string code)
+        {
+            if (string.IsNullOrEmpty(code)) return;
+            Id = code;
+            if (Id == "llu") return;
+
+            var vals = code.Split('_');
+            if (code.Length < 3) return;
+
+            FType = vals[0];
+            TopWindows = int.Parse(vals[1]);
+            BottomWindows = int.Parse(vals[2]);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WinInsolGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+            path = System.IO.Path.GetFullPath(path + @"..\..\..\..\..\example\one_string.txt");
+            
+            
+            //read file
+            var levelCodes = System.IO.File.ReadAllLines(path);
+            foreach (var levelCode  in levelCodes)
+            {
+                var level = GetFlatsFromCode(levelCode);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="levelCode"></param>
+        /// <param name="sideWindowsQty">количество окон в торце</param>
+        /// <returns></returns>
+        private static List<Flat> GetFlatsFromCode(string levelCode, int sideWindowsQty)
+        {
+            if (string.IsNullOrEmpty(levelCode)) return null;
+            var codes = levelCode.Trim().Split(';');
+
+            //total number of windows
+            for (int i = 0; i < codes.Length; i++)
+            {
+                if (codes[i].Contains("CR))
+            }
+        }
+    }
+}
