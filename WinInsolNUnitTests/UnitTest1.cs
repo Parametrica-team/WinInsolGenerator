fa39acd91225b06eeea7aae6b52732e50a60ee70
Max <mmm@parametrica.team>
2020-10-22 22:11:56 +0300

    добавлены тесты

--- WinInsolNUnitTests/UnitTest1.cs
+++ WinInsolNUnitTests/UnitTest1.cs
@@ -25,8 +25,8 @@ namespace WinInsolNUnitTests
                 Indexes = new List<int>() { 0, 1, 2, 3, 4 }
             };
             
-            var result = flat.GetWindowCombinations();
-            var res = string.Join('_', result);
+            flat.SetWindowCombinations();
+            var res = string.Join('_', flat.WindowCombinations);
             Assert.AreEqual(res, "0,1_0,2_0,3_0,4_1,2_1,3_1,4_2,3_2,4_3,4");
         }
 
@@ -41,8 +41,8 @@ namespace WinInsolNUnitTests
                 Indexes = new List<int>() { 0, 1, 2, 3}
             };
 
-            var result = flat.GetWindowCombinations();
-            var res = string.Join('_', result);
+            flat.SetWindowCombinations();
+            var res = string.Join('_', flat.WindowCombinations);
             Assert.AreEqual(res, "0_1_2_3");
         }
 
@@ -57,9 +57,16 @@ namespace WinInsolNUnitTests
                 Indexes = new List<int>() { 0, 1, 2, 3, 4, 5}
             };
 
-            var result = flat.GetWindowCombinations();
-            var res = string.Join('_', result);
+            flat.SetWindowCombinations();
+            var res = string.Join('_', flat.WindowCombinations);
             Assert.AreEqual(res, "0,1,2_0,1,3_0,1,4_0,1,5_0,2,3_0,2,4_0,2,5_0,3,4_0,3,5_0,4,5_1,2,3_1,2,4_1,2,5_1,3,4_1,3,5_1,4,5_2,3,4_2,3,5_2,4,5_3,4,5");
+        }
+
+        [TestCase(5, "CL_1_1,MD_0_1,MD_0_1,MD_0_1,CR_0_1,llu,MU_1_0,CR_1_0")]
+        [TestCase(13, "CL_1_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,MD_0_1,CR_1_1,MU_2_0,MU_1_0,MU_1_0,llu,MU_1_0,MU_2_0,MU_1_0")]
+        public void GetStepsFromCode(int res, string code)
+        {
+            Assert.AreEqual(res, Program.GetStepsFromCode(code));
         }
     }
 }
