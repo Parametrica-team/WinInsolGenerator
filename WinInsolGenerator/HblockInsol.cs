a5d8cb8252bdcb3b65011dd0a996ed7a3d45d4e6
Max <mmm@parametrica.team>
2020-10-22 22:12:26 +0300

    Добавлены WinInsol и HBlockInsol

---
+++ WinInsolGenerator/HblockInsol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinInsolGenerator
+{
+    /// <summary>
+    /// Здесь хранятся списки подходящих уровней для каждой комбинации инсоляции для HBlock
+    /// </summary>
+    public class HblockInsol
+    {
+        /// <summary>
+        /// Тип секции
+        /// </summary>
+        public string HblockId { get; set; }
+        /// <summary>
+        /// Словарь winInsol - список уровней для каждой комбинации инсоляции
+        /// </summary>
+        public Dictionary<string, HashSet<string>> InsolData { get; set; }        
+
+        public HblockInsol()
+        {
+            InsolData = new Dictionary<string, HashSet<string>>();
+        }
+
+        public HblockInsol(string hblockId)
+        {
+            HblockId = hblockId;
+            InsolData = new Dictionary<string, HashSet<string>>();
+        }
+    }
+
+}
---
+++ WinInsolGenerator/WindowInsol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    public class WindowInsol
+    {
+        public string HblockId { get; set; }
+        public string llu { get; set; }
+        public List<string> WindowId { get; set; }
+
+        public WindowInsol()
+        {
+        }
+    }
+}
